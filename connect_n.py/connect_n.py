61217e793e4118c2602656dbfe3cdbc3b638566c
Detergent4316 <maharshi@midasit.in>
2025-02-15 12:42:26 +0530

    Plan Connect N

---
+++ connect_n.py/connect_n.py
@@ -0,0 +1,22 @@
+'''
+Ask for clarifying questions
+Found out following points
+1. Played player vs player
+2. Board can be of variable dimensions
+3. Game can be of connecting 'n' discs in a row (horizontally, vertically or diagonally)
+4. Score tracking system for players
+
+
+######## Future Scope ##########
+1. Connect it with Postgresql database to store scores for players
+'''
+
+'''
+
+### High level design
+
+1. Board class
+2. Player class
+3. Game class
+4. Game class manages state of the board and turn of players
+'''
